b3225787873f8da59c647632a7fd59e7b699bd49
Prashant Kumar Yadav <kartik224blue@gmail.com>
2022-05-16 12:36:06 +0530

    implimented genres based recommendation

--- views/home.py
+++ views/home.py
@@ -4,9 +4,12 @@ import pandas as pd
 import requests
 
 movies_dict = pickle.load(open('./assets/data/movies.pkl','rb'))
+genres_with_movieId=pickle.load(open('./assets/data/genres_with_movieId.pkl','rb'))
+crew_with_movieId=pickle.load(open('./assets/data/crew_avgrating.pkl','rb'))
 similarity = pickle.load(open('./assets/data/similarity.pkl','rb'))
-cast_with_avgrating=pickle.load(open('./assets/data/cast_avgrating.pkl','rb'))
 crew_names=pickle.load(open('./assets/data/crew_names.pkl','rb'))
+genres_name=pickle.load(open('./assets/data/genres_name.pkl','rb'))
+
 movies=pd.DataFrame(movies_dict) 
 
 # Fetch poster from API 
@@ -22,14 +25,34 @@ def fetch_poster_and_title(movie_id):
 
 # Function for recommendation of next k movies by crew name
 def recommend_movies_by_crew_name(crew_name,no_of_recommendations):
-    movies_list=cast_with_avgrating[crew_name]
-    movies_list=movies_list.sort_values(ascending=False)
+    moviesIds_list=crew_with_movieId[crew_name]
+    moviesIds_list=moviesIds_list.sort_values(ascending=False)
     movie_posters=[]
     recommended_movies=[]
     counter=0
     loader=st.image('./assets/gif/96x96.gif')
     loding_text=st.text("loading.....")
-    for i in movies_list.index:
+    for i in moviesIds_list.index:
+        if(counter==no_of_recommendations+1):
+            break
+        title,path=(fetch_poster_and_title(i))
+        movie_posters.append(path)
+        recommended_movies.append(title)
+        counter=counter+1
+    loader.empty()
+    loding_text.empty()
+    return recommended_movies,movie_posters
+
+# Function for recommendation of next k movies by g name
+def recommend_movies_by_genres_name(genres_name,no_of_recommendations):
+    moviesIds_list=genres_with_movieId[genres_name]
+    moviesIds_list=moviesIds_list.sort_values(ascending=False)
+    movie_posters=[]
+    recommended_movies=[]
+    counter=0
+    loader=st.image('./assets/gif/96x96.gif')
+    loding_text=st.text("loading.....")
+    for i in moviesIds_list.index:
         if(counter==no_of_recommendations+1):
             break
         title,path=(fetch_poster_and_title(i))
@@ -61,6 +84,8 @@ def recommend_movies_by_movie_name(movie,no_of_movies):
 def generate_cards(name,no_of_movies,type):
     if type=="Movie based":
         movies_Names,movies_Poster=recommend_movies_by_movie_name(name,no_of_movies)
+    elif type=="genres based":
+        movies_Names,movies_Poster=recommend_movies_by_genres_name(name,no_of_movies)
     else :
         movies_Names,movies_Poster=recommend_movies_by_crew_name(name,no_of_movies)
 
@@ -71,20 +96,25 @@ def generate_cards(name,no_of_movies,type):
             counter=0
         with col[counter]:
             st.text(movies_Names[i-1])
+            st.write("[know more](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
             st.image(movies_Poster[i-1])
             counter=counter+1
 def load_view():
     st.title("See what's Next")
-    category = ['--Select--', 'Movie based', 'Person based(Crew Member)']
+    category = ['--Select--', 'Movie based', 'Person based(Crew Member)','genres based']
     category_opt = st.selectbox('Select Recommendation Type', category)
     if category_opt==category[1]:
         option = st.selectbox(
         'How would you like to be search?',
         movies['title'].values)
-    else:
+    elif category_opt==category[2]:
         option = st.selectbox(
         'How would you like to be search?',
         crew_names)
+    else:
+        option = st.selectbox(
+        'How would you like to be search?',
+        genres_name)
     
     no_of_movies = st.slider('Number of movies you want Recommended:', min_value=5, max_value=20, step=1)
     if st.button('Search'):
