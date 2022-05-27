00f8ea1310f1a60e5d1e86abcf15c915d2d9028e
Prashant Kumar Yadav <kartik224blue@gmail.com>
2022-05-27 16:39:47 +0530

    edit requirements.txt

--- views/about.py
+++ views/about.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import time
 def load_view():    
     st.title('About Page')
     st.write("An avid movie lover or a movie fanatic? PMDB is your go to app whenever you're confused between movies. Just type in some keywords and get the perfect movie according to your mood.Whether it's an inspiring sci-fi, thriller, drama, or biography, PMDB will always have a suitable film.With the rating system of our app, you can get recommendations on the basis of your own ratings, and we,ll show you the top rated movies of your taste. We at pmdb provide users with different recommendation types to choose from, ranging from the movie type, actors, directors, etc.Users can create their accounts in a jiff and we,ll help them pacify their movie experience.")
